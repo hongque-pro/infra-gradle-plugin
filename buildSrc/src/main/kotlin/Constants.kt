35fbe8731866702f24a3527ce76cd78bbe2b7bbd
endink <sharping@outlook.com>
2024-07-26 16:01:32 +0800

    upgrade ksp oom

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -5,7 +5,7 @@ object Constants {
     const val publishingPluginVersion = "1.3.0"
     const val gradlePublishPluginVersion = "1.2.1"
     const val testPlugin = false
-    const val kspPluginVersion = "1.9.21-1.0.16"
+    const val kspPluginVersion = "1.9.21+"
     const val mybatisPluginVersion = "2.4.1"
     const val mybatisPluginLib = "gradle.plugin.com.thinkimi.gradle:mybatis-generator-plugin"
     const val checkUpdatePlugin = "0.50.0"
